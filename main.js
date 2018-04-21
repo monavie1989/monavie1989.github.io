72fd00de7d2329aba67ece5470d3774eb584db46
unknown <quyet@magingam.com>
2018-04-21 11:19:53 +0700

    update code

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+const socket = io('https://webrtcmagingam.herokuapp.com/');
+
+const peer = new Peer({ 
+    key: 'peerjs', 
+    host: 'peerjsmagingam.herokuapp.com', 
+    secure: true, 
+    port: 443
+});
+var myPeerId;
+var myStream;
 function openStream(){
 	const config = {audio: false, video: true};
 	return navigator.mediaDevices.getUserMedia(config);
