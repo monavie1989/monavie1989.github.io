6e73c12565fc311b547042f4b35478f8ecc6919b
unknown <quyet@magingam.com>
2018-04-21 10:27:32 +0700

    update code

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var myPeerId;
 peer.on('open', id => {
 	myPeerId = id;
 	const username = makeid();
-    $('#my-peer').append(id+"-"+username);
+    $('#my-peer').append(username);
 	socket.emit('NGUOI_DUNG_DANG_KY', { ten: username, peerId: id });
 });
 
@@ -18,13 +18,13 @@ socket.on('DANH_SACH_ONLINE', arrUserInfo => {
     arrUserInfo.forEach(user => {
         const { ten, peerId } = user;
 		if(myPeerId != peerId){
-			$('#online_list').append(`<div id="${peerId}"><h3 id="my-peer">User Id: ${peerId}-${ten}</h3><video id="remoteStream${peerId}" width="300" controls></video></div>`);
+			$('#online_list').append(`<div id="${peerId}"><h3 id="my-peer">User Name: ${ten}</h3><video id="remoteStream${peerId}" width="300" controls></video></div>`);
 		}
     });
 
     socket.on('CO_NGUOI_DUNG_MOI', user => {
         const { ten, peerId } = user;
-        $('#online_list').append(`<div id="${peerId}"><h3 id="my-peer">User Id: ${peerId}-${ten}</h3><video id="remoteStream${peerId}" width="300" controls></video></div>`);
+        $('#online_list').append(`<div id="${peerId}"><h3 id="my-peer">User Name: ${ten}</h3><video id="remoteStream${peerId}" width="300" controls></video></div>`);
     });
 
     socket.on('AI_DO_NGAT_KET_NOI', peerId => {
