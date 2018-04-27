0db75a2f84aa088ee22034967686c4b256ce5f39
unknown <quyet@magingam.com>
2018-04-27 10:43:44 +0700

    update code

--- main.js
+++ main.js
@@ -1,50 +1,21 @@
-const socket = io('https://webrtcmagingam.herokuapp.com/');
+const socket = io('https://stream3005.herokuapp.com/');
 
-const peer = new Peer({ 
-    key: 'peerjs', 
-    host: 'peerjsmagingam.herokuapp.com', 
-    secure: true, 
-    port: 443
-});
-var myPeerId;
-var myStream;
-function openStream(){
-	const config = {audio: false, video: true};
-	return navigator.mediaDevices.getUserMedia(config);
-}
-function playStream(idVideoTag, stream){
-	const video = document.getElementById(idVideoTag);
-	video.srcObject = stream;
-	video.play();
-}
-openStream().then(function(stream){
-	playStream('localStream',stream);
-	myStream = stream;
-	console.log(stream);
-});
-peer.on('open', id => {
-	myPeerId = id;
-	const username = makeid();
-    $('#my-peer').append(username);
-	
-	console.log("myPeerId:"+myPeerId);
-	socket.emit('NGUOI_DUNG_DANG_KY', { ten: username, peerId: id });
-});
+$('#div-chat').hide();
+
+
+
 socket.on('DANH_SACH_ONLINE', arrUserInfo => {
+    $('#div-chat').show();
+    $('#div-dang-ky').hide();
+
     arrUserInfo.forEach(user => {
         const { ten, peerId } = user;
-		if(myPeerId != peerId){			
-			$('#online_list').append(`<div id="${peerId}"><h3 id="my-peer">User Name: ${ten}</h3><video id="remoteStream${peerId}" width="300" controls></video></div>`);
-			
-			const call = peer.call(peerId, myStream);
-			call.on('stream', remoteStream => playStream('remoteStream'+peerId, remoteStream));
-			
-		}
+        $('#ulUser').append(`<li id="${peerId}">${ten}</li>`);
     });
 
     socket.on('CO_NGUOI_DUNG_MOI', user => {
         const { ten, peerId } = user;
-        $('#online_list').append(`<div id="${peerId}"><h3 id="my-peer">User Name: ${ten}</h3><video id="remoteStream${peerId}" width="300" controls></video></div>`);
+        $('#ulUser').append(`<li id="${peerId}">${ten}</li>`);
     });
 
     socket.on('AI_DO_NGAT_KET_NOI', peerId => {
@@ -54,17 +25,65 @@ socket.on('DANH_SACH_ONLINE', arrUserInfo => {
 
 socket.on('DANG_KY_THAT_BAT', () => alert('Vui long chon username khac!'));
 
+
+function openStream() {
+    const config = { audio: false, video: true };
+    return navigator.mediaDevices.getUserMedia(config);
+}
+
+function playStream(idVideoTag, stream) {
+    const video = document.getElementById(idVideoTag);
+    video.srcObject = stream;
+    video.play();
+}
+
+// openStream()
+// .then(stream => playStream('localStream', stream));
+
+const peer = new Peer({ 
+    key: 'peerjs', 
+    host: 'peerjsmagingam.herokuapp.com', 
+    secure: true, 
+    port: 443
+});
+
+
+peer.on('open', id => {
+    $('#my-peer').append(id);
+    $('#btnSignUp').click(() => {
+        const username = $('#txtUsername').val();
+        socket.emit('NGUOI_DUNG_DANG_KY', { ten: username, peerId: id });
+    });
+});
+
+//Caller
+$('#btnCall').click(() => {
+    const id = $('#remoteId').val();
+    openStream()
+    .then(stream => {
+        playStream('localStream', stream);
+        const call = peer.call(id, stream);
+        call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+    });
+});
+
 //Callee
 peer.on('call', call => {
-	call.answer(myStream);
-	call.on('stream', remoteStream => playStream('remoteStream'+call.peer, remoteStream));
+    openStream()
+    .then(stream => {
+        call.answer(stream);
+        playStream('localStream', stream);
+        call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+    });
 });
-function makeid() {
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for (var i = 0; i < 5; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-  return text;
-}
+$('#ulUser').on('click', 'li', function() {
+    const id = $(this).attr('id');
+    console.log(id);
+    openStream()
+    .then(stream => {
+        playStream('localStream', stream);
+        const call = peer.call(id, stream);
+        call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+    });
+});
