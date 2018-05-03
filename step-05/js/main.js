d28081ca0efa92dd4ebfc2f268350f9b61627626
unknown <quyet@magingam.com>
2018-05-03 16:53:24 +0700

    update step-05

--- step-05/js/main.js
+++ step-05/js/main.js
@@ -26,7 +26,7 @@ var room = 'foo';
 // Could prompt for room name:
 // room = prompt('Enter room name:');
 
-var socket = io.connect();
+var socket = io.connect("https://webrtcmagingam.herokuapp.com/");
 
 if (room !== '') {
   socket.emit('create or join', room);
