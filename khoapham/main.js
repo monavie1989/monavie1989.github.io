7d6b9c4066717f2c0c91a52dd246fddd0ea93f3d
monavie1989 <nguyennhuquyet@gmail.com>
2018-05-23 17:06:39 +0700

    Update main.js

--- khoapham/main.js
+++ khoapham/main.js
@@ -36,7 +36,7 @@ socket.on('DANH_SACH_ONLINE', arrUserInfo => {
         };
         $('#ulUser').append(`<li id="${user.ten}">${user.ten}<br><video width="300" controls></video></li>`);
     });
-    //socket.emit('NGUOI_DUNG_DANG_KY_SUCCESS', { ten: username , peer_list: peers[user.ten]});
+    socket.emit('NGUOI_DUNG_DANG_KY_SUCCESS', { ten: username , peer_list: peers});
     console.log("NGUOI_DUNG_DANG_KY_SUCCESS");
     console.log(peers);
 });
