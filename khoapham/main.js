e0f33f0a12e39df85a7842bfd111bcbde458dd22
monavie1989 <nguyennhuquyet@gmail.com>
2018-06-01 17:04:14 +0700

    Update main.js

--- khoapham/main.js
+++ khoapham/main.js
@@ -2,8 +2,8 @@ const socket = io('webrtcmagingam.herokuapp.com');
 var peers = [];
 //$('#div-chat').hide();
 function openStream() {
-    const config = { audio: true, video: false };
-    //const config = { audio: false, video: true };
+    //const config = { audio: true, video: false };
+    const config = { audio: true, video: true };
     return navigator.mediaDevices.getUserMedia(config);
 }
 
